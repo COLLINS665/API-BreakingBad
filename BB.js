ab0d3a9409fa7cd8ecebe83d4e7665b1419a493e
collins665 <kimaiyocollins78@gmail.com>
2022-02-12 11:35:55 +0000

    working with APIs

---
+++ BB.js
@@ -0,0 +1,15 @@
+let quote = document.querySelector(".quote")
+let quotegen = document.querySelector(".quote_gen")
+
+getquote = ()=>{
+    fetch("https://breakingbadapi.com/api/quotes")
+    .then(res => res.json())
+    .then(data =>{
+        // console.log(data)
+        let quoted = Math.floor(Math.random() * data.length)
+        quote.innerHTML = `${data[quoted].author + " : " + data[quoted].quote}`
+    })
+}
+
+quotegen.addEventListener('click',getquote)
+
